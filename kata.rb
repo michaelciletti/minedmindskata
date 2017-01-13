25b8bd8d5880f68f42e80c5b19d403810cc96aa2
michaelciletti <michaelciletti@gmail.com>
2017-01-13 12:47:06 -0500

    print 1-10 verticaly

--- kata.rb
+++ kata.rb
@@ -1,13 +1,14 @@
-10. times do
+# 10. times do
 
 # puts "1"
 
-end
-
-# for tt in 1..10
-# 	print tt, " \n";
 # end
 
-for a in 1..100
-	puts a, "\n";
+for tt in 1..10
+	print tt, " \n";
 end
+
+# for a in 1..100
+	# puts a, "\n";
+
+# end
